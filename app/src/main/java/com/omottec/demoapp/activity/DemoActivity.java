efdde8a5946a03c6bdb5e6b982d8d7d3e07d249c
omottec <omottec@gmail.com>
2016-03-31 00:15:05 +0800

    抽取SingleFragmentActivity基类

---
+++ app/src/main/java/com/omottec/demoapp/activity/DemoActivity.java
@@ -0,0 +1,13 @@
+package com.omottec.demoapp.activity;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by qinbingbing on 3/31/16.
+ */
+public class DemoActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return null;
+    }
+}
