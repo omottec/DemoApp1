cdfe3e6eda36ac69f62f7cafb30f9fbb14ebbfc4
omottec <omottec@gmail.com>
2016-03-31 16:56:55 +0800

    process memory leak caused by thread and handler

--- app/src/main/java/com/omottec/demoapp/activity/LeakActivity.java
+++ app/src/main/java/com/omottec/demoapp/activity/LeakActivity.java
@@ -1,12 +1,14 @@
 package com.omottec.demoapp.activity;
 
-import android.graphics.Color;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.FragmentActivity;
-import android.widget.TextView;
 
 import com.omottec.demoapp.R;
 
+import java.lang.ref.WeakReference;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -14,27 +16,112 @@ import java.util.concurrent.TimeUnit;
  */
 public class LeakActivity extends FragmentActivity {
     private byte[] largeObj = new byte[10 * 1024 * 1024];
+    private StrongRefHandler mHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.full_screen_text);
-        TextView tv = (TextView) findViewById(R.id.tv);
-        tv.setBackgroundColor(Color.GREEN);
-        tv.setText("LeakActivity");
-        new LeakClass().start();
+//        new LeakThread().start();
+//        new StaticThread().start();
+//        new StaticStrongRefThread(this).start();
+//        new StaticWeakRefThread(this).start();
+        mHandler = new StrongRefHandler(this);
+        mHandler.sendMessageDelayed(Message.obtain(), 10 * 60 * 1000);
     }
 
-    class LeakClass extends Thread {
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mHandler.removeCallbacksAndMessages(null);
+    }
+
+    class LeakThread extends Thread {
         @Override
         public void run() {
-            while (true) {
-                try {
-                    TimeUnit.HOURS.sleep(1);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+            try {
+                TimeUnit.HOURS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
+        }
+    }
+
+    static class StaticThread extends Thread {
+        @Override
+        public void run() {
+            try {
+                TimeUnit.HOURS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    static class StaticStrongRefThread extends Thread {
+        private Context context;
+
+        StaticStrongRefThread(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        public void run() {
+            try {
+                TimeUnit.HOURS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            // Do Something with Context
+        }
+    }
+
+    static class StaticWeakRefThread extends Thread {
+        WeakReference<Context> contextRef;
+        StaticWeakRefThread(Context context) {
+            contextRef = new WeakReference<Context>(context);
+        }
+
+        @Override
+        public void run() {
+            try {
+                TimeUnit.HOURS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Context context = contextRef.get();
+            if (context != null)
+                ; // Do Something
+        }
+    }
+
+    class StrongRefHandler extends Handler {
+        Context context;
+
+        public StrongRefHandler(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            // Do Something with Context
+        }
+    }
+
+    static class StaticWeakRefHandler extends Handler {
+        WeakReference<Context> contextRef;
+
+        public StaticWeakRefHandler(Context context) {
+            contextRef = new WeakReference<Context>(context);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            Context context = contextRef.get();
+            if (context != null)
+                ; // Do Something with Context
         }
     }
 }
